5b51efbee9682020cfb05d2dcade5782ea4359e6
rakshith <rakshit.s.gowda@gmail.com>
2024-12-27 18:43:21 +0530

    old

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,24 @@
+import Properties from '@/components/Properties'
 import PropertySearchForm from '@/components/PropertySearchForm'
-import properties from '@/properties_example.json'
 
 const PropertiesPage = () => {
   return (
     <>
-      {properties.length === 0 ? (
-        <p>no property found</p>
-      ) : (
-        <div>{properties.map((property) => property.name)}</div>
-      )}
+      <section className='bg-blue-700 py-5'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section classname='bg-blue-700 py-5'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+          <div className=''>
+            {/* <Properties /> */}
+            {Properties.map((property) => (
+              <div>{property.name}</div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   )
 }
--- components/Properties.jsx
+++ components/Properties.jsx
@@ -1,11 +1,14 @@
 // client component
 'use client'
 
+import PropertyCard from './PropertyCard'
+
 const Properties = () => {
   return (
     <div>
       Properties
       {/* properties will have a card ui element */}
+      <PropertyCard />
       {/* pagination element */}
     </div>
   )
